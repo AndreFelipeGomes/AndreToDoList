06affb1d783d806932643a55ae65c0a1a0c84d09
AndreFelipeGomes <andrefelipe_bg@outlook.com>
2020-03-02 19:20:18 -0300

    Screen Authentication

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import 'react-native-gesture-handler';
+import { TextInput } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import Authentication from './screens/screenAuthentication';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class MyComponent extends React.Component {
+  state = {
+    user: '',
+    password: '',
+    auth: false,
+  };
+
+  _validAuth = () => {
+    (this.state.user == 'ANDRE')?
+      (this.state.password == '123')? 
+       this.setState({auth : true})
+      :
+       this.setState({auth : false})
+    : 
+      this.setState({auth : false})
+  }
+  render(){
+    return (
+      <Authentication>
+
+      </Authentication>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'rgb(34, 34, 34)',
   },
-});
+})
